097811f33a634223c318eb1747e6145f9a56e807
Jinsoo Yoon <floreo1242@gmail.com>
2024-05-21 22:28:50 +0900

    Initial Commit

---
+++ bouquet/src/main/java/com/dongguk/csc40281/securezone/bouquet/BouquetApplication.java
@@ -0,0 +1,13 @@
+package com.dongguk.csc40281.securezone.bouquet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouquetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BouquetApplication.class, args);
+    }
+
+}
---
+++ bouquet/src/test/java/com/dongguk/csc40281/securezone/bouquet/BouquetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongguk.csc40281.securezone.bouquet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouquetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

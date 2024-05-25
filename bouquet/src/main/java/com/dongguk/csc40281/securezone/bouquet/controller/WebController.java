e024e44585b6013abe6be1d5f6ff9f6a51b85257
Jinsoo Yoon <floreo1242@gmail.com>
2024-05-26 02:59:58 +0900

    web mapping 완료

--- bouquet/src/main/java/com/dongguk/csc40281/securezone/bouquet/controller/WebController.java
+++ bouquet/src/main/java/com/dongguk/csc40281/securezone/bouquet/controller/WebController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
+    @GetMapping("/")
+    public String index() {
+        return "main";
+    }
+
     @GetMapping("/join")
     public String join() {
         return "join_membership";
@@ -14,6 +19,51 @@ public class WebController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/account")
+    public String account() {
+        return "account";
+    }
+
+    @GetMapping("/search")
+    public String find() {
+        return "search_result";
+    }
+
+    @GetMapping("/add")
+    public String add() {
+        return "add_new_product";
+    }
+
+    @GetMapping("/customer/center")
+    public String customerCenter() {
+        return "customer_center";
+    }
+
+    @GetMapping("/withdrawal")
+    public String withdrawal() {
+        return "membership_withdrawal";
+    }
+
+    @GetMapping("/pay")
+    public String pay() {
+        return "pay";
+    }
+
+    @GetMapping("pay/result")
+    public String payResult() {
+        return "payment_result";
+    }
+
+    @GetMapping("/product")
+    public String product() {
+        return "product_list";
+    }
+
+    @GetMapping("/basket")
+    public String basket() {
+        return "shopping_basket";
     }
 
 }

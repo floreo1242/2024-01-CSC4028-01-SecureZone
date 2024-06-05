20bce4963de91a9f4653cee90f17410caf4e90cc
Jinsoo Yoon <floreo1242@gmail.com>
2024-06-05 13:47:27 +0900

    back: 로그인 검증 메세지 삭제

--- bouquet/src/main/java/com/dongguk/csc40281/securezone/bouquet/dto/LoginRequestDto.java
+++ bouquet/src/main/java/com/dongguk/csc40281/securezone/bouquet/dto/LoginRequestDto.java
@@ -9,18 +9,12 @@ import lombok.Data;
 public class LoginRequestDto {
 
     @NotBlank
-    @Pattern(
-            regexp = "^[a-zA-Z0-9]{5,12}$",
-            message = "아이디는 5~12자의 영문 대소문자와 숫자로만 구성되어야 합니다."
-    )
+    @Pattern(regexp = "^[a-zA-Z0-9]{5,12}$")
     private String username;
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, max = 16, message = "Password must be 8 ~ 16 characters long")
-    @Pattern(
-            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,16}$",
-            message = "비밀번호는 8~16자의 영문, 숫자, 특수문자를 포함해야 합니다."
-    )
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*]).{8,16}$")
     private String password;
 
 }
